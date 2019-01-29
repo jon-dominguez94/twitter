418b02884397aead5bac7cc0b13105455e673315
jondominguez94 <jon.dominguez94@yahoo.com>
2019-01-29 08:31:29 -0800

    Setup passport

--- app.js
+++ app.js
@@ -1,25 +1,65 @@
+// const express = require("express");
+// const app = express();
+
+// const db = require("./config/keys").mongoURI;
+// const mongoose = require("mongoose");
+
+// mongoose
+//   .connect(db, { useNewUrlParser: true })
+//   .then(() => console.log("Connected to MongoDB successfully"))
+//   .catch(err => console.log(err));
+
+// const passport = require("passport");
+// app.use(passport.initialize());
+// require('./config/passport')(passport);
+
+// const bodyParser = require("body-parser");
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+
+// const users = require("./routes/api/users");
+// const tweets = require("./routes/api/tweets");
+
+// app.use("/api/users", users);
+// app.use("/api/tweets", tweets);
+
+// const port = process.env.PORT || 5000;
+// app.listen(port, console.log(`Server is running on port ${port}`));
+
+const bodyParser = require("body-parser");
+const passport = require("passport");
+
 const express = require("express");
 const app = express();
 
-const db = require("./config/keys").mongoURI;
+// const path = require('path');
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static('frontend/build'));
+//   app.get('/', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
+
+
 const mongoose = require("mongoose");
+const db = require('./config/keys').mongoURI;
+
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-const users = require("./routes/api/users");
-const tweets = require("./routes/api/tweets");
+app.use(passport.initialize());
+require('./config/passport')(passport);
 
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
 
-app.get('/', (req, res) => res.send("Yo World"));
-
 const port = process.env.PORT || 5000;
-app.listen(port, console.log(`Server is running on port ${port}`));
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
