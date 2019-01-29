ea68ee80fdd02a0548d6330c45431650fd3219a2
jondominguez94 <jon.dominguez94@yahoo.com>
2019-01-29 09:14:27 -0800

    Write validations for login and register

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,9 @@ const User = require("../../models/User");
 const keys = require("../../config/keys");
 const passport = require('passport');
 
+const validateRegisterInput = require("../../validation/register");
+const validateLoginInput = require("../../validation/login");
+
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
 
 router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
@@ -17,18 +20,18 @@ router.get('/current', passport.authenticate('jwt', { session: false }), (req, r
 });
 
 router.post('/register', (req, res) => {
-  // const { errors, isValid } = validateRegisterInput(req.body);
+  const { errors, isValid } = validateRegisterInput(req.body);
   
-  // if(!isValid){
-  //   return res.status(400).json(errors);
-  // }
+  if(!isValid){
+    return res.status(400).json(errors);
+  }
 
   User.findOne({ email: req.body.email })
   .then(user => {
     if(user){
-      // errors.email = "A user has already registered with this email";
-      // return res.status(400).json(errors);
-      return res.status(400).json({ email: 'A user has already registered with this email' });
+      errors.email = "A user has already registered with this email";
+      return res.status(400).json(errors);
+      // return res.status(400).json({ email: 'A user has already registered with this email' });
     } else {
       const newUser = new User({
         handle: req.body.handle,
@@ -60,11 +63,11 @@ router.post('/register', (req, res) => {
 
 router.post('/login', (req, res) => {
 
-  // const { errors, isValid } = validateLoginInput(req.body);
+  const { errors, isValid } = validateLoginInput(req.body);
 
-  // if(!isValid){
-  //   return res.status(400).json(errors);
-  // }
+  if(!isValid){
+    return res.status(400).json(errors);
+  }
 
   const email = req.body.email;
   const password = req.body.password;
@@ -72,9 +75,9 @@ router.post('/login', (req, res) => {
   User.findOne({ email })
   .then(user => {
     if(!user){
-      // errors.email = "This user does not exist";
-      // return res.status(404).json(errors);
-      return res.status(404).json({ email: 'This user does not exist' });
+      errors.email = "This user does not exist";
+      return res.status(404).json(errors);
+      // return res.status(404).json({ email: 'This user does not exist' });
     } else {
       bcrypt.compare(password, user.password)
       .then(isMatch => {
@@ -89,9 +92,9 @@ router.post('/login', (req, res) => {
             }
           );
         } else {
-          // errors.password = "Incorrect Password";
-          // return res.status(400).json(errors);
-          return res.status(400).json({ password: 'Password is incorrect' });
+          errors.password = "Incorrect Password";
+          return res.status(400).json(errors);
+          // return res.status(400).json({ password: 'Password is incorrect' });
         }
       });
     }
