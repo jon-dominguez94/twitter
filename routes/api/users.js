a65c9ee2593d8e2e3310fe26e589dc043dc7693c
jondominguez94 <jon.dominguez94@yahoo.com>
2019-01-29 07:10:06 -0800

    Write login route for user

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const User = require("../../models/User");
 
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
@@ -25,6 +26,27 @@ router.post('/register', (req, res) => {
           .then(user => res.json(user))
           .catch(err => console.log(err));
         });
+      });
+    }
+  });
+});
+
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email })
+  .then(user => {
+    if(!user){
+      return res.status(404).json({ email: 'This user does not exist' });
+    } else {
+      bcrypt.compare(password, user.password)
+      .then(isMatch => {
+        if(isMatch){
+          res.json({ msg: 'Success'});
+        } else {
+          return res.status(400).json({ password: 'Password is incorrect' });
+        }
       });
     }
   });
